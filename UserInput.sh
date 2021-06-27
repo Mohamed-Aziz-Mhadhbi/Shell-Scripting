37e7f6435234f3c06666c040816927674d86d7ac
Mohamed-Aziz-Mhadhbi <mohamedaziz.mhadhbi@esprit.tn>
2021-06-28 00:04:56 +0100

    if&else if

--- UserInput.sh
+++ UserInput.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+#User Input
 read -p "Enter your name: " NAME 
 
 echo "You said your name was $NAME"
---
+++ if_Statment.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+read -p "Please Enter Your Name " Name
+
+if [ "$Name" = "Aziz" ];
+then
+    echo "Welcome Aziz"
+else
+    echo "Your are not Aziz"
+fi
